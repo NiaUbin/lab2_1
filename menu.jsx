1237e2806a1f50d917d585eba07985bc9303c368
Nai-Nattawat <119363901+NiaUbin@users.noreply.github.com>
2025-11-07 10:22:13 +0700

    gu commit Ja

--- menu.jsx
+++ menu.jsx
@@ -4,9 +4,18 @@ export default function menu() {
   return (
     <div>
         <div>
-            <h1>Menu</h1>
+            <h1>Menu1</h1>
+            <p>We offer a wide range of menu items to choose from.</p>
+        </div>
+       <div>
+            <h1>Menu2</h1>
+            <p>We offer a wide range of menu items to choose from.</p>
+        </div>
+       <div>
+            <h1>Menu3</h1>
             <p>We offer a wide range of menu items to choose from.</p>
         </div>
     </div>
+    
   )
 }
