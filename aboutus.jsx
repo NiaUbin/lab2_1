7e89576f868090b8145fe1074958f3165246ec82
NiaUbin <aofsang44@gmail.com>
2025-11-07 09:59:49 +0700

    commit2

---
+++ aboutus.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function aboutus() {
+  return (
+    <div>
+        <div>
+            <h1>About Us</h1>
+            <p>We are a team of developers who are passionate about creating web applications that are easy to use and efficient.</p>
+        </div>
+    </div>
+  )
+}
---
+++ menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function menu() {
+  return (
+    <div>
+        <div>
+            <h1>Menu</h1>
+            <p>We offer a wide range of menu items to choose from.</p>
+        </div>
+    </div>
+  )
+}
